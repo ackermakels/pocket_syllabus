95e6c1c99ac136cd6f8f37210620797f294e5d90
oxnersteve <59854206+oxnersteve@users.noreply.github.com>
2021-03-31 11:36:18 -0400

    Added to the add course layout, began to build out the AddCourse class

--- app/src/main/java/com/example/pocketsyllabus/MainActivity.java
+++ app/src/main/java/com/example/pocketsyllabus/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.pocketsyllabus;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) { OpenAddNewCourseActivity();}
+        });
     }
+
+    public void OpenAddNewCourseActivity(){
+
+        Intent i1 = new Intent(this, AddCourse.class);
+        startActivity(i1);
+        Toast.makeText(this, "Opening Add New Course Page", Toast.LENGTH_SHORT).show();
+    }
+
+
 }
